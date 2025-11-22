56ce13a3f17f5e6ce64f45787e114f7337eb8996
getrohit97 <getrohit97@gmail.com>
2025-11-22 14:23:50 +0530

    converted to dev and prod env

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,26 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import productRoutes from "./routes/product.routes.js"
+import productRoutes from "./routes/product.routes.js";
+import path from "path";
 
 dotenv.config();
-const app=express();
+const app = express();
 app.use(express.json());
 
 connectDB();
-const PORT=process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-app.use("/api",productRoutes)
-app.listen(PORT,()=>{
-      console.log(`Server running on port ${PORT}`)
+app.use("/api", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+});
+}
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
